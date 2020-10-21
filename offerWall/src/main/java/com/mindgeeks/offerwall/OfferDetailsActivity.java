eaaac0b728fd751753d191cdc070a986eef6ae44
mamoon123 <moonasus558@gmail.com>
2020-10-21 13:03:28 +0530

    2nd change

---
+++ offerWall/src/main/java/com/mindgeeks/offerwall/OfferDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.mindgeeks.offerwall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfferDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_details);
+    }
+}
